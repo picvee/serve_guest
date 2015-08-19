ba9ca301a2a2a07a852be246c8b21a89527d5d6f
Anushree1991 <anu.girish58@gmail.com>
2015-08-19 15:16:07 +0530

    new changes

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,7 @@
 class Menu < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_and_belongs_to_many :items
+  belongs_to :account
 
   def json_attributes
     {name: self.name, items: self.items.collect{|item| item.json_attributes}}
